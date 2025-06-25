28ce74c6dc009e8550a3ae294ae5bbb61fc09e43
sohel rana <ranaot56@gmail.com>
2025-06-25 21:07:14 +0600

    ♻️ refactor(route): centralize file upload and data parsing logic
    - introduce `uploadAndParse` middleware to handle file uploads and parsing
    - remove duplicated logic from blog and project routes
    - improve code maintainability and reduce redundancy

--- src/module/Blog/blog.routes.ts
+++ src/module/Blog/blog.routes.ts
@@ -5,6 +5,7 @@ import upload from "../../middlewares/multer";
 import AppError from "../../middlewares/AppError";
 import status from "http-status";
 import uploadBufferToCloudinary from "../../utils/uploadToCloudinary";
+import { uploadAndParse } from "../../middlewares/uploadAndParse";
 
 const router = express.Router();
 
@@ -13,37 +14,7 @@ router.get("/", BlogControllers.getAllBlogs);
 router.post(
   "/",
   upload.array("files"),
-
-  async (req, res, next) => {
-    try {
-      const files = req.files as Express.Multer.File[];
-
-      if (!files || files.length === 0) {
-        throw new AppError(status.BAD_REQUEST, "No file uploaded");
-      }
-
-      const uploadPromise = files.map((file) =>
-        uploadBufferToCloudinary(file.buffer, "projects")
-      );
-
-      const uploadResults = await Promise.all(uploadPromise);
-      const imageUrls = uploadResults.map((result: any) => result.secure_url);
-
-      //  parse
-      if (req.body.data && typeof req.body.data === "string") {
-        req.body.data = JSON.parse(req.body.data);
-      }
-
-      req.body = {
-        ...req.body.data,
-        imageUrls: imageUrls,
-      };
-
-      next();
-    } catch (error) {
-      next(error);
-    }
-  },
+  uploadAndParse("blogs", "imageUrls", true),
   auth("OWNER"),
   BlogControllers.createBlog
 );
@@ -53,36 +24,7 @@ router.get("/deleted", auth("OWNER"), BlogControllers.getAllDeletedBlogs);
 router.patch(
   "/:id",
   upload.array("files"),
-  async (req, res, next) => {
-    try {
-      const files = req.files as Express.Multer.File[];
-
-      let imageUrls: string[] = [];
-
-      if (files && files.length > 0) {
-        const uploadPromise = files.map((file) =>
-          uploadBufferToCloudinary(file.buffer, "projects")
-        );
-
-        const uploadResults = await Promise.all(uploadPromise);
-        imageUrls = uploadResults.map((result: any) => result.secure_url);
-      }
-
-      //  parse
-      if (req.body.data && typeof req.body.data === "string") {
-        req.body.data = JSON.parse(req.body.data);
-      }
-
-      req.body = {
-        ...req.body.data,
-        ...(imageUrls.length > 0 && { imageUrls }),
-      };
-
-      next();
-    } catch (error) {
-      next(error);
-    }
-  },
+  uploadAndParse("blogs", "imageUrls", true),
   auth("OWNER"),
   BlogControllers.updateBlog
 );
--- src/module/Project/project.route.ts
+++ src/module/Project/project.route.ts
@@ -9,6 +9,7 @@ import AppError from "../../middlewares/AppError";
 import status from "http-status";
 import path from "path";
 import uploadBufferToCloudinary from "../../utils/uploadToCloudinary";
+import { uploadAndParse } from "../../middlewares/uploadAndParse";
 
 const router = express.Router();
 
@@ -18,48 +19,8 @@ router.get("/deleted", auth("OWNER"), ProjectControllers.getDeletedProjects);
 router.post(
   "/",
   upload.array("files"),
+  uploadAndParse("projects", "images", true),
 
-  async (req, res, next) => {
-    try {
-      const files = req.files as Express.Multer.File[];
-
-      if (!files || files.length === 0) {
-        throw new AppError(status.BAD_REQUEST, "No file uploaded");
-      }
-
-      const uploadPromise = files.map((file) =>
-        uploadBufferToCloudinary(file.buffer, "projects")
-      );
-
-      const uploadResults = await Promise.all(uploadPromise);
-      const imageUrls = uploadResults.map((result: any) => result.secure_url);
-
-      //  parse
-      if (req.body.data && typeof req.body.data === "string") {
-        req.body.data = JSON.parse(req.body.data);
-      }
-
-      // parse tags and keyFeatures inside data,
-      if (req.body.data.tags && typeof req.body.data.tags === "string") {
-        req.body.data.tags = JSON.parse(req.body.data.tags);
-      }
-      if (
-        req.body.data.keyFeatures &&
-        typeof req.body.data.keyFeatures === "string"
-      ) {
-        req.body.data.keyFeatures = JSON.parse(req.body.data.keyFeatures);
-      }
-
-      req.body = {
-        ...req.body.data,
-        images: imageUrls,
-      };
-
-      next();
-    } catch (error) {
-      next(error);
-    }
-  },
   validateRequest(projectSchema.CreateProjectSchema),
 
   auth("OWNER"),
@@ -70,52 +31,11 @@ router.post(
 router.patch(
   "/:id",
   upload.array("files"),
+  uploadAndParse("projects", "images", true),
 
-  async (req, res, next) => {
-    try {
-      const files = req.files as Express.Multer.File[];
-
-      let imageUrls: string[] = [];
-
-      if (files && files.length > 0) {
-        const uploadPromise = files.map((file) =>
-          uploadBufferToCloudinary(file.buffer, "projects")
-        );
-        const uploadResults = await Promise.all(uploadPromise);
-        imageUrls = uploadResults.map((result: any) => result.secure_url);
-      }
-
-      //  Parse `data` JSON string if present
-      if (req.body.data && typeof req.body.data === "string") {
-        req.body.data = JSON.parse(req.body.data);
-      }
-
-      // Parse tags and keyFeatures if passed as JSON strings
-      if (req.body.data?.tags && typeof req.body.data.tags === "string") {
-        req.body.data.tags = JSON.parse(req.body.data.tags);
-      }
-
-      if (
-        req.body.data?.keyFeatures &&
-        typeof req.body.data.keyFeatures === "string"
-      ) {
-        req.body.data.keyFeatures = JSON.parse(req.body.data.keyFeatures);
-      }
-
-      req.body = {
-        ...req.body.data,
-        ...(imageUrls.length > 0 && { images: imageUrls }),
-      };
-
-      next();
-    } catch (error) {
-      next(error);
-    }
-  },
-
-  validateRequest(projectSchema.UpdateProjectSchema), // 6. Validate schema
-  auth("OWNER"), // 7. Auth middleware
-  ProjectControllers.updateProject // 8. Final handler
+  validateRequest(projectSchema.UpdateProjectSchema),
+  auth("OWNER"),
+  ProjectControllers.updateProject
 );
 
 router.delete(
